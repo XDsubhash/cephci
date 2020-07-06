5a375c42c549392ea08cafa3c77031e32c5d7109
subhash polisetti <vpoliset@redhat.com>
2020-07-07 04:09:59 +0530

    Update utils.py

--- utility/utils.py
+++ utility/utils.py
@@ -601,7 +601,7 @@ def email_results(results_list, run_id, send_to_cephci=False):
         text_file = open("result.props", "wt")
         text_file.write(emailmsg)
         text_file.close()
-        subprocess.call(["export", "run_status=", "{}".format(run_status)])
+        subprocess.call("export run_status={}".format(run_status))
         subprocess.call('echo run_status="{}" >> result.props'.format(run_status))
         subprocess.call("echo result_status='`cat results.html`' >> result.props")
         
